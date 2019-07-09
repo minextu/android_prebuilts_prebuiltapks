90f4d296c1d5c9504c6ff2bb0924e226f799536f
chris42 <list-christian@web.de>
2019-07-09 23:10:41 +0200

    Fixed typo in apk name

--- AppleNlpBackend/Android.mk
+++ AppleNlpBackend/Android.mk
@@ -19,7 +19,7 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := AppleNlpBackend
-LOCAL_SRC_FILES := org.microg.nlp.backend.apple
+LOCAL_SRC_FILES := org.microg.nlp.backend.apple.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
